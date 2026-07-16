4dd146871ababde5dbd4b0a8cc9c5088bfde693f
gdddd <77157311+gdd-xxx@users.noreply.github.com>
2026-07-16 21:10:11 +0800

    fix: 配置QQ AppID/Token/UserID

--- app/config.py
+++ app/config.py
@@ -28,7 +28,7 @@ DEFAULT_CONFIG = {
     "use_custom_api": False,
     "push_channels": {
         "wechat_webhook": "",
-        "qq_bot": {"app_id": "", "token": "", "user_id": "", "channel_id": ""},
+        "qq_bot": {"app_id": "1905200617", "token": "F7zsmgbWSPMKIHGGHIKMPSWbgmsz6ENW", "user_id": "2506020285", "channel_id": ""},
         "feishu_webhook": "",
     },
     "fetch_interval": 60,
